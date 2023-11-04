c5915dc818d79b23e0269ebbc79530f0fac57cee
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-05 01:16:14 +0700

    Modifying Templates and Create Hello World to Web

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bikini_bottom.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home)
 ]
