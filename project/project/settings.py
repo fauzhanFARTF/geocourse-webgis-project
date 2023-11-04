992f88208d2aa563c0384d3edeb0470baaad2bb5
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-04 20:38:30 +0700

    Modifying INSTALLED_APPS in file setting.py, Create Custom Apps

--- project/project/settings.py
+++ project/project/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Custom Apps
+    'bikini_bottom',
 ]
 
 MIDDLEWARE = [
