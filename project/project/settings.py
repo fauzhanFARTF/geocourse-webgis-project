4523cdaf925fc44e619e822dbe1ae15b4d79c5ee
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-04 18:15:22 +0700

    Modifying SECRET KEY in file setting.py, to file .env

--- project/project/settings.py
+++ project/project/settings.py
@@ -11,16 +11,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os, environ
+
+# Environment Settings
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3a94kb&21cv6-l#he9&n#y=f6phixy3qcrs5dw5rbs7*(%27c8'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
