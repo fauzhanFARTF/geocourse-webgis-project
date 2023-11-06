c1ae52b3af5c6c96c5b4e9aa36005afb1e669623
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-06 21:42:52 +0700

    Create Model (Class Facility Admin, Admin Register Facility)

--- project/bikini_bottom/admin.py
+++ project/bikini_bottom/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+# from django.contrib import admin      @#Admin Non Spatial
+from django.contrib.gis  import admin   # Admin Spatial
+from .models import Facility
 
 # Register your models here.
+class LocationAdmin(admin.OSMGeoAdmin):
+    default_zoom = 11
+    default_lon = 18409015.29
+    default_lat = 1300740.59
+
+@admin.register(Facility)
+class FacilityAdmin(LocationAdmin):
+    list_filter = ['status', 'operator'] 
+    list_display = ['id', 'name', 'status', 'operator']
+
+# admin.site.register(Facility)
+# class FacilityAdmin(LocationAdmin):
+#     list_filter = ['status', 'operator']
+#     list_display = ['id', 'name', 'status', 'operator']
