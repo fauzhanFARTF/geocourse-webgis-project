b39e276a63bc68b93839544bffd91d4294e67796
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-15 08:42:23 +0700

    Create Static Root, Static URL, Media Root, dan Media URL

--- project/project/settings.py
+++ project/project/settings.py
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "assets/static"
+MEDIA_URL = 'media/'
+MEDIA_ROOT = "assets/media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- project/project/urls.py
+++ project/project/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from bikini_bottom.views import home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
