7a4853fbbdc7eb85f75bae09aaf6ec380c3be5cf
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-11-19 17:13:06 +0700

    Step 4 - Create Custom API

--- project/bikini_bottom/views.py
+++ project/bikini_bottom/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.core.serializers import serialize  #melakukan serialisasi menghasilkan data geojson
 from .models import Facility 
-from django.http import HttpResponse 
+from django.http import HttpResponse ,JsonResponse
+import ast
 
 # Create your views here.
 def home(request):
@@ -10,3 +11,46 @@ def home(request):
 def home_map_api(request):
     data = serialize('geojson', Facility.objects.all())
     return HttpResponse(data, content_type="json") 
+
+def custom_map_api(request):
+    features = {
+        'type': 'FeatureCollection',
+        'crs' : {
+            'type': 'name',
+            'properties': {
+                'name' : 'EPSG:4326'
+            },
+        },
+        'features' : []
+    }
+    
+    model = Facility.objects.all()
+    for item in model :
+        # print(item.name)
+        # feature = { 
+            # 'nama' : item.name,
+            # 'tipe' : item.types,
+            # 'harga' : item. price,
+            # 'satuan_harga' : item.price_unit
+        # }
+        feature = {
+            'type' :'Feature',
+            'geometry': ast.literal_eval(item.location.json),
+            'properties' : 
+                {
+                    'nama' : item.name,
+                    'tipe' : item.types,
+                    'harga' : item.price,
+                    'satuan_harga' : item.price_unit
+                },
+        }
+        features['features'].append(feature)
+    # print(features)
+    return JsonResponse(features, safe = False)
+    
+    # data = {
+    #     'nama':'faiz',
+    #     'usia': 28,
+    #     'perempuan': False
+    #     }
+    # return JsonResponse (data)
--- project/project/urls.py
+++ project/project/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from bikini_bottom.views import home, home_map_api
+from bikini_bottom.views import home, home_map_api, custom_map_api
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
-    path('api/home-map/', home_map_api),
+    path('api/home-map/', home_map_api, name='home_api'),
+    path('api/custom-api/', custom_map_api, name='custom_api'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
