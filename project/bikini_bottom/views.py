aeff50867d690d5085e2afd9e8c7cfb7945b132e
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-12-07 00:15:41 +0700

    Step 6 - Setup Facility List

--- project/bikini_bottom/views.py
+++ project/bikini_bottom/views.py
@@ -73,3 +73,9 @@ def facility_form_add(request):
        'form' : form 
     }
     return render(request,'pages/facility_add.html', context)
+
+def facility_list(request):
+    context = {
+        'data' : Facility.objects.all()
+    }
+    return render(request,'pages/facility_list.html', context)
