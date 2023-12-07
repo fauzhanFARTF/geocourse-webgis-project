6d12abf1195d96ccf1c226b2c99f5116720bd74d
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-12-07 10:59:03 +0700

    Step 7 - Sistem Authentication

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,6 +37,10 @@ ALLOWED_HOSTS = env('HOSTS').split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    # Custom Apps
+    'bikini_bottom',
+    
+    # Djanggo Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -51,9 +55,6 @@ INSTALLED_APPS = [
     # Crispy Form
     "crispy_forms",
     "crispy_bootstrap5",
-    
-    # Custom Apps
-    'bikini_bottom',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'project.urls'
+LOGIN_REDIRECT_URL = '/'
 
 TEMPLATES = [
     {
