67d5b8e47dbbead4fa5e31b815323877675a623a
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-12-07 10:52:27 +0700

    Create Sistem Authentication part 4

--- project/project/settings.py
+++ project/project/settings.py
@@ -74,7 +74,7 @@ LOGIN_REDIRECT_URL = '/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
