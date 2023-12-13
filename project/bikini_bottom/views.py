d84da3c81562cc3f108b3b5f9c5fe37166cea609
Fauzan Nurrachman <62664091+fauzhanFARTF@users.noreply.github.com>
2023-12-13 14:36:34 +0700

    Step 8 - Update & Delete Object Facility

--- project/bikini_bottom/views.py
+++ project/bikini_bottom/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.core.serializers import serialize  #melakukan serialisasi menghasilkan data geojson
 from .models import Facility 
 from .forms import FacilityForm
@@ -74,8 +74,36 @@ def facility_form_add(request):
     }
     return render(request,'pages/facility_add.html', context)
 
+def facility_form_update(request , pk):
+    objek = get_object_or_404(Facility, id=pk)
+    form = FacilityForm(request.POST or None, request.FILES or None, instance=objek)
+    
+    if request.method == 'POST':
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.operator = request.user
+            data.save()
+            return redirect('facility_list')
+    context = {
+        'form' : form
+    }
+    return render(request,'pages/facility_update.html', context)
+    
+def facility_form_delete(request, pk):
+    objek = get_object_or_404(Facility, id=pk)
+    form = FacilityForm(request.POST or None, request.FILES or None, instance=objek)
+    
+    if request.method == 'POST':
+        objek.delete()
+        return redirect('facility_list')
+    context = {
+        'form' : form
+    }
+    return render(request, 'pages/facility_delete.html', context)
+
 def facility_list(request):
     context = {
-        'data' : Facility.objects.all()
+        # 'data' : Facility.objects.all()
+        'data' : Facility.objects.filter(operator = request.user)
     }
     return render(request,'pages/facility_list.html', context)
